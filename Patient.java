28e88725c0becda28f7acb1d92757560a6408bcb
Joshua Foo <82205276+josshhz11@users.noreply.github.com>
2024-11-11 19:12:37 +0800

    Update Patient Menu (Patient.java)

--- Patient.java
+++ Patient.java
@@ -93,14 +93,15 @@ public class Patient extends User {
         System.out.println("""
                 Patient Display Menu: 
                 1. View Medical Record
-                2. Update Contact Information
+                2. Update Personal Information
                 3. View Available Appointment Slots
-                4. Schedule Appointment
-                5. View Scheduled Appointments
-                6. Reschedule Appointment
-                7. Cancel Appointment
-                8. Logout
-                Choose options (1-8):
+                4. Schedule an Appointment
+                5. Reschedule an Appointment
+                6. Cancel an Appointment
+                7. View Scheduled Appointments
+                8. View Past Appointment Outcome Records
+                9. Logout
+                Choose options (1-9):
                 """);
     }
 }
