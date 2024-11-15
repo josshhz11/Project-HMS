230b23e0ed038b590e33d17dede0a9fe46bd4d51
ichinggg <127404794+ichinggg@users.noreply.github.com>
2024-11-15 22:25:09 +0800

    Update MedicalRecord.java minor edit add serializable to first line

--- HMS-Current (1)/MedicalRecord.java
+++ HMS-Current (1)/MedicalRecord.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class MedicalRecord {
+public class MedicalRecord implements Serializable{
     private static final long serialVersionUID = 1L;
     private String patientID;
     private String bloodType;
