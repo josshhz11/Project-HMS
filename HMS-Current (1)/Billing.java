ee2c3104772032045943458269689ca6d9e85f08
clarabelle04 <clarabelle.chua@gmail.com>
2024-11-16 22:17:05 +0800

    Update Billing.java

--- HMS-Current (1)/Billing.java
+++ HMS-Current (1)/Billing.java
@@ -42,6 +42,20 @@ public class Billing{
       billingItems.add(item);
       totalAmount += cost; 
 
+        private class BillingItem{
+    	private String description;
+    	private double cost;
+    	
+    	public BillingItem(String description, double cost) {
+    		this.description = description;
+    		this.cost = cost;
+    	}
+        @Override
+        public String toString() {
+            return description + ": $" + cost;
+        }
+
+    }
 
     
 
